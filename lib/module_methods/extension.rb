186d592544e750efba7a0607f6fef947e4ff0a8a
Alexander Popov <alex.wayfer@gmail.com>
2020-07-06 16:15:18 +0300

    Improve documentation

--- lib/module_methods/extension.rb
+++ lib/module_methods/extension.rb
@@ -2,12 +2,18 @@
 
 module ModuleMethods
 	## Module for module methods, because this module can be included
-	## into other modules before including into controllers.
+	## into other modules before including into classes.
 	module Extension
+		## Getter for saving the whole chain of including and extending (nested `ClassMethods`)
 		def modules_with_class_methods
 			@modules_with_class_methods ||= [self]
 		end
 
+		## Main logic, which will be applied after extending with this module
+		## @param base [Module, Class, nil] a module or a class in which extended module
+		##   will be included
+		## @yield a block just will be passed to the `super`;
+		##   it's for `ActiveSupport::Concern` compatibility
 		def included(base = nil, &block)
 			super
 
